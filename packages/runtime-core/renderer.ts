de7fa8668279d3f984635a35fbfdd900b6bfa261
komoroi <koheim2511@gmail.com>
2024-01-28 23:30:41 +0900

    second-commit

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,24 +1,59 @@
+import { VNode } from './vnode'
+
 export type RootRenderFunction<HostElement = RendererElement> = (
-    message: string,
-    container: HostElement,
-  ) => void
-  
-  export interface RendererOptions<HostNode = RendererNode> {
-    setElementText(node: HostNode, text: string): void
-  }
-  
-  export interface RendererNode {
-    [key: string]: any
-  }
-  
-  export interface RendererElement extends RendererNode {}
-  
-  export function createRenderer(options: RendererOptions) {
-    const { setElementText: hostSetElementText } = options
-  
-    const render: RootRenderFunction = (message, container) => {
-      hostSetElementText(container, message)
+  vnode: VNode,
+  container: HostElement,
+) => void
+
+export interface RendererOptions<
+  HostNode = RendererNode,
+  HostElement = RendererElement,
+> {
+  patchProp(el: HostElement, key: string, value: any): void
+
+  createElement(type: string): HostElement
+
+  createText(text: string): HostNode
+
+  setElementText(node: HostNode, text: string): void
+
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void
+}
+
+export interface RendererNode {
+  [key: string]: any
+}
+
+export interface RendererElement extends RendererNode {}
+
+export function createRenderer(options: RendererOptions) {
+  const {
+    patchProp: hostPatchProp,
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    insert: hostInsert,
+  } = options
+
+  function renderVNode(vnode: VNode | string) {
+    if (typeof vnode === 'string') return hostCreateText(vnode)
+    const el = hostCreateElement(vnode.type)
+
+    Object.entries(vnode.props).forEach(([key, value]) => {
+      hostPatchProp(el, key, value)
+    })
+
+    for (const child of vnode.children) {
+      const childEl = renderVNode(child)
+      hostInsert(childEl, el)
     }
-  
-    return { render }
+
+    return el
   }
+
+  const render: RootRenderFunction = (vnode, container) => {
+    const el = renderVNode(vnode)
+    hostInsert(el, container)
+  }
+
+  return { render }
+}
